ee4e351fde2a8d3d50ab301365c8d541172bfacb
Linus Rundberg Streuli <schizoakustik@schizoakustik.se>
2014-10-29 13:50:32 +0100

    Add Football
    Add a simple Football game.

---
+++ Assets/Script/BallControllerScript.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControllerScript : MonoBehaviour {
+	private int Player1Score = 0;
+	private int Player2Score = 0;
+	public GameObject gameController;
+
+	public GUIText Player1ScoreText;
+	public GUIText Player2ScoreText;
+	public GUIText DebugText;
+	private bool Player2;
+	private float minY = -10f;
+	public float bouncyBallTimer;
+	//	private float lastTime = 0f;
+	public float bounce = .8f;
+	public float powerUpSpeed = 50f;
+	public Sprite[] powerUpSprites;
+
+	void Awake() {
+		//	powerUpSprites = Resources.LoadAll<Sprite>("gundux");
+		gameController = GameObject.Find("GameControllerObject");
+	}
+
+	// Use this for initialization
+	void Start () {
+		Player1Score = 0;
+		Player2Score = 0;
+		collider2D.sharedMaterial.bounciness = bounce;
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "washingMachine1") {
+			Player2 = true;
+			Player2Score++;
+			UpdateScore();
+		}
+
+		if (other.gameObject.tag == "washingMachine2") {
+			Player2 = false;
+			Player1Score++;
+			UpdateScore(); }
+
+		if (other.gameObject.tag == "Player") {
+			//	lastHitBy = other.gameObject;
+			//	DebugText.text = "Last hit by " + lastHitBy.name;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "powerUp") {
+			Destroy(other.gameObject);
+			//	DebugText.text = "Hit " + other.gameObject.name;
+
+			if (other.gameObject.name == "bouncyBallPrefab(Clone)") {
+				BouncyBall();
+			}
+
+			if (other.gameObject.name == "playerSpeedUpPrefab(Clone)") {
+				PlayerSpeedUp();
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//	DebugText.text = "x: " + transform.position.x.ToString() + " y: " + transform.position.y.ToString();
+		if(transform.position.y < minY) {
+			transform.position = new Vector3(0, 0, 0);
+			rigidbody2D.velocity = new Vector2(0, 0);
+		}
+
+		if((Time.time - bouncyBallTimer) > 15f) {
+			collider2D.sharedMaterial.bounciness = bounce;
+			DebugText.text = "";
+		}
+	}
+
+	void UpdateScore() {
+		GameObject P1 = gameController.GetComponent<GameControllerScript>().Player1;
+		GameObject P2 = gameController.GetComponent<GameControllerScript>().Player2;
+		if (Player2 == true) {
+			Player2ScoreText.text = Player2Score.ToString();
+		}
+		else
+			Player1ScoreText.text = Player1Score.ToString();
+
+		transform.position = new Vector3(0, 0, 0);
+		rigidbody2D.velocity = new Vector2(0, 0);
+		P1.transform.position = new Vector2(-0.8f, -0.8f);
+		P2.transform.position = new Vector2(1f, -0.8f);
+		P1.transform.rotation = new Quaternion(0, 0, 0, 0);
+		P2.transform.rotation = new Quaternion(0, 0, 0, 0);
+		
+	}
+
+	void BouncyBall() {
+		DebugText.text = "It's BOUNCY BALL!";
+		collider2D.sharedMaterial.bounciness = 1f;
+		bouncyBallTimer = Time.time;
+		//Invoke("EndBouncyBall", 15);
+	}
+
+	/* void EndBouncyBall() {
+		collider2D.sharedMaterial.bounciness = bounce;
+		DebugText.text = "No more BOUNCY BALL...";
+	} */
+
+	void PlayerSpeedUp() {
+		
+		Debug.Log(gameController.GetComponent<GameControllerScript>().p1MaxSpeed);
+		
+	//	Debug.Log("Player 1: " + GameObject.Find("Player1").GetComponent<PlayerControllerScript>().maxSpeed);
+	//	Debug.Log("Player 2: " + GameObject.Find("Player2").GetComponent<PlayerControllerScript>().maxSpeed);
+		
+	//	Debug.Log("Before: " + lastHitBy.GetComponentInChildren<SpriteRenderer>().sprite);
+	//	lastHitBy.GetComponentInChildren<SpriteRenderer>().sprite = powerUpSprites[0];
+	//	Debug.Log("After: " + lastHitBy.GetComponentInChildren<SpriteRenderer>().sprite);
+		
+		//DebugText.text = lastHitBy.name + " speed: " + lastHitBy.GetComponent<PlayerControllerScript>().maxSpeed;
+		
+		
+	
+	}
+
+}
---
+++ Assets/Script/GameControllerScript.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerScript : MonoBehaviour {
+	//PLAYERS
+	public GameObject Player1;
+	public GameObject Player2;
+	public float p1MaxSpeed = 2f;
+	public float p2MaxSpeed = 2f;
+	public float jumpForce = 125f;
+	public bool p1CanJump = true;
+	public bool p2CanJump = true;
+
+	public bool p1Grounded = false;
+	public bool p2Grounded = false;
+	public Transform p1GroundCheck;
+	public Transform p2GroundCheck;
+	
+	float groundRadius = 0.2f;
+	
+	public LayerMask whatIsGround;
+
+	bool p1DoubleJump = false;
+	bool p2DoubleJump = false;
+	
+	//POWER UPS
+/* 	public float powerUpSpawningTime = 15f;
+	private float lastTime = 0f; */
+
+	public GUIText debugText;
+//	public GameObject[] powerUpPrefabs;
+	//static Camera mainCamera = Camera.main;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void FixedUpdate() {
+		//	debugText.text = Time.time.ToString();
+		p1Grounded = Physics2D.OverlapCircle(p1GroundCheck.position, groundRadius, whatIsGround);
+		p2Grounded = Physics2D.OverlapCircle(p2GroundCheck.position, groundRadius, whatIsGround);
+
+		if(p1Grounded)
+			p1DoubleJump = false;
+
+		if(p2Grounded)
+			p2DoubleJump = false;
+
+		float p1Move = Input.GetAxis("P1Horizontal");
+		float p2Move = Input.GetAxis("P2Horizontal");
+		Player1.rigidbody2D.velocity = new Vector2(p1Move * p1MaxSpeed, Player1.rigidbody2D.velocity.y);
+		Player2.rigidbody2D.velocity = new Vector2(p2Move * p2MaxSpeed, Player2.rigidbody2D.velocity.y);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+		if((p1Grounded || !p1DoubleJump) && Input.GetButtonDown("P1Jump") && p1CanJump) {
+			Player1.rigidbody2D.AddForce(new Vector2(0, jumpForce));
+
+			if(!p1DoubleJump && !p1Grounded)
+				p1DoubleJump = true;
+		}
+		
+		if((p2Grounded || !p2DoubleJump) && Input.GetButtonDown("P2Jump") && p2CanJump){
+			Player2.rigidbody2D.AddForce(new Vector2(0, jumpForce));
+
+			if(!p2DoubleJump && !p2Grounded)
+				p2DoubleJump = true;
+		}
+		
+	/* 	if((Time.time - lastTime) > powerUpSpawningTime) {
+			lastTime = Time.time;
+			float maxX = Random.Range(-15, 13);
+			float maxY = Random.Range(4, 10);
+			spawnPowerUp(maxX, maxY);
+		} */
+	}
+
+	/* void spawnPowerUp(float maxX, float maxY) {
+		int randomPowerUp = Random.Range(0, powerUpPrefabs.Length);
+		Vector2 randomPos = new Vector2(maxX, maxY);
+		Instantiate(powerUpPrefabs[randomPowerUp], randomPos, transform.rotation);
+		} */
+
+}
